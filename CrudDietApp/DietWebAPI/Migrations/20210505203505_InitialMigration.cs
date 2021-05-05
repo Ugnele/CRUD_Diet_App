b8ad2ab5b985b41bf262a7f8a5905d5ea7c04905
Ugnele <ugne.sarakauskaite@gmail.com>
2021-05-05 21:38:53 +0100

    Renamed DietWebAPI project to CrudDietAPI and DietWebLibrary to CrudDietLibrary for convenience. Modified startup.cs configureservices method, so that it would accept lower case urls, and also woule be able to accept mysql databases. Then, created data folder with 'ApplicationDbContext' class where I have created datatables for users and recipes. Added an initial migration

---
+++ CrudDietApp/DietWebAPI/Migrations/20210505203505_InitialMigration.cs
@@ -0,0 +1,67 @@
+﻿using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace CrudDietAPI.Migrations
+{
+    public partial class InitialMigration : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Users",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("MySql:ValueGenerationStrategy", MySqlValueGenerationStrategy.IdentityColumn),
+                    Username = table.Column<string>(type: "longtext CHARACTER SET utf8mb4", nullable: true),
+                    Password = table.Column<string>(type: "longtext CHARACTER SET utf8mb4", nullable: true),
+                    Email = table.Column<string>(type: "longtext CHARACTER SET utf8mb4", nullable: true),
+                    Age = table.Column<int>(type: "int", nullable: false),
+                    About = table.Column<string>(type: "longtext CHARACTER SET utf8mb4", nullable: true),
+                    PictureUrl = table.Column<string>(type: "longtext CHARACTER SET utf8mb4", nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Users", x => x.Id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Recipes",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("MySql:ValueGenerationStrategy", MySqlValueGenerationStrategy.IdentityColumn),
+                    Title = table.Column<string>(type: "longtext CHARACTER SET utf8mb4", nullable: true),
+                    Method = table.Column<string>(type: "longtext CHARACTER SET utf8mb4", nullable: true),
+                    Ingredients = table.Column<string>(type: "longtext CHARACTER SET utf8mb4", nullable: true),
+                    PictureUrl = table.Column<string>(type: "longtext CHARACTER SET utf8mb4", nullable: true),
+                    Type = table.Column<int>(type: "int", nullable: false),
+                    CreatedById = table.Column<int>(type: "int", nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Recipes", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_Recipes_Users_CreatedById",
+                        column: x => x.CreatedById,
+                        principalTable: "Users",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Restrict);
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Recipes_CreatedById",
+                table: "Recipes",
+                column: "CreatedById");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Recipes");
+
+            migrationBuilder.DropTable(
+                name: "Users");
+        }
+    }
+}
--- CrudDietApp/DietWebAPI/Startup.cs
+++ CrudDietApp/DietWebAPI/Startup.cs
@@ -1,7 +1,9 @@
+using DietWebAPI.Data;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.HttpsPolicy;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
@@ -26,7 +28,9 @@ namespace DietWebAPI
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
-
+            services.AddRouting(r => r.LowercaseUrls = true);
+            var connectionString = Configuration.GetConnectionString("DefaultConnection");
+            services.AddDbContext<ApplicationDbContext>(options => options.UseMySql(connectionString, ServerVersion.AutoDetect(connectionString)));
             services.AddControllers();
             services.AddSwaggerGen(c =>
             {
