ca8af739a22eecdf2cc50bc43d4c40ef26637759
Ugnele <ugne.sarakauskaite@gmail.com>
2021-05-08 09:13:45 +0100

    Added tests for controllers in UserControllerTest and RecipeControllerTest classes, deleted unnecessary interfaces

--- CrudDietApp/CrudDietTests/Controllers/RecipeControllerTest.cs
+++ CrudDietApp/CrudDietTests/Controllers/RecipeControllerTest.cs
@@ -1,10 +1,14 @@
 ﻿using CrudDietApp.Controllers;
 using CrudDietLibrary.Interfaces;
 using CrudDietLibrary.Models;
+using CrudDietLibrary.Models.Binding;
+using CrudDietLibrary.Models.View;
 using Microsoft.AspNetCore.Mvc;
 using Moq;
+using Spring.Collections;
 using System;
 using System.Collections.Generic;
+using System.Linq;
 using Xunit;
 
 namespace CrudDietTests
@@ -12,108 +16,79 @@ namespace CrudDietTests
     public class RecipeControllerTest
     {
         private Mock<IRepositoryWrapper> mockRepo;
-        private RecipesController recipesController;
-        //private CreateRecipe createRecipe;
-        //private UpdateRecipe updateRecipe;
-        private Recipe recipe;
-        private List<Recipe> recipes;
-        private Mock<IRecipe> recipeMock;
-        private List<IRecipe> recipesMock;
-        //private Mock<IAddRecipe> addRecipeMock;
-        //private Mock<IUpdateRecipe> updateRecipeMock;
-        //private Mock<IRecipeViewModel> RecipeViewModelMock;
-        //private List<IRecipeViewModel> RecipesViewModelMock;
+        private RecipesController recipeController;
+        private AddRecipeBindingModel addRecipe;
+
         public RecipeControllerTest()
         {
-            //mock setup
-            recipeMock = new Mock<IRecipe>();
-            recipesMock = new List<IRecipe> { recipeMock.Object };
+            //sample models
+            addRecipe = new AddRecipeBindingModel { Title = "Recipe Added", Method = "Cook", Ingredients = "Food", Type = DietType.Vegan, PictureUrl = "" };
 
-            recipe = new Recipe();
-            recipes = new List<Recipe>();
-
-            //controller set up
-            var recipeResultsMock = new Mock<IActionResult>();
+            //controller setup
+            mockRepo = new Mock<IRepositoryWrapper>();
+            recipeController = new RecipesController(mockRepo.Object);
         }
-        //        addRecipeMock = new Mock<IAddRecipe>();
-        //        updateRecipeMock = new Mock<IUpdateRecipe>();
-        //        
-        //        //viewmodels mock setup
-        //        RecipeViewModelMock = new Mock<IRecipeViewModel>();
-        //        RecipesViewModelMock = new List<IRecipeViewModel>();
 
-        //        //sample models
-        //        addRecipe = new AddRecipe { Code = "CS101", Title = "Computing Basics" };
-        //        updateRecipe = new UpdateRecipe { Code = "CS101", Title = "Understanding Computing Basics" };
-
-        //        CONTROLLER SETUP//controller setup
-        //         recipeControllerMock = new Mock<IRecipesController>();
-        //        _logger = new Mock<ILogger<RecipeController>>();
-
-        //        mockRepo = new Mock<IRepositoryWrapper>();
-        //        var allRecipes = GetRecipes();
-        //        RecipeController = new RecipeController(_logger.Object, mockRepo.Object);
-        //    }
         [Fact]
-        public void GetAllRecipes_Test()
+        public void CreateRecipe_Test()
         {
-            mockRepo.Setup(repo => repo.Recipes.FindAll()).Returns(GetRecipes());
-            var controllerActionResult = recipesController.Index();
+            mockRepo.Setup(repo => repo.Recipes.FindByCondition(c => c.Id == It.IsAny<int>())).Returns(GetRecipes());
+            var controllerActionResult = recipeController.CreateRecipe(addRecipe);
             Assert.NotNull(controllerActionResult);
         }
 
         [Fact]
-        public void AddRecipe_Test()
+        public void GetAllRecipes_Test()
         {
-            mockRepo.Setup(repo => repo.Recipes.FindByCondition(r => r.Id == It.IsAny<int>())).Returns(GetRecipes());
-            //var controllerActionResult = RecipesController.Post(addRecipe);
-            //Assert.NotNull(controllerActionResult);
-            //Assert.IsType<ActionResult<RecipeViewModel>>(controllerActionResult);
+            mockRepo.Setup(repo => repo.Recipes.FindAll()).Returns(GetRecipes());
+            var controllerActionResult = recipeController.Index();
+            Assert.NotNull(controllerActionResult);
+            Assert.IsType<ViewResult>(controllerActionResult);
         }
+
         [Fact]
-        public void DeleteRecipe_Test()
+        public void GetRecipe_Test()
         {
-            //Arrange
-            mockRepo.Setup(repo => repo.Recipes.FindByCondition(r => r.Id == It.IsAny<int>())).Returns(GetRecipes());
-            //mockRepo.Setup(repo => repo.Recipes.Delete(GetRecipe()));
-            ////Act
-            //var controllerActionResult = RecipesController.Delete(It.IsAny<int>());
-            ////Assert
-            //Assert.NotNull(controllerActionResult);
+            mockRepo.Setup(repo => repo.Recipes.FindAll()).Returns(GetRecipes());
+            var controllerActionResult = recipeController.DetailsOfRecipe(It.IsAny<int>());
+            Assert.NotNull(controllerActionResult);
+            Assert.IsType<ViewResult>(controllerActionResult);
         }
+
+        [Fact]
+        public void UpdateRecipe_Test()
+        {
+            mockRepo.Setup(repo => repo.Recipes.FindByCondition(r => r.Id == It.IsAny<int>())).Returns(GetRecipes());
+            mockRepo.Setup(repo => repo.Recipes.Update(GetRecipe()));
+            var controllerActionResult = recipeController.UpdateRecipe(It.IsAny<int>());
+            Assert.NotNull(controllerActionResult);
+            Assert.IsType<ViewResult>(controllerActionResult);
+
+        }
+
+        [Fact]
+        //public void DeleteRecipe_Test()
+        //{
+        //    mockRepo.Setup(repo => repo.Recipes.FindByCondition(r => r.Id == It.IsAny<int>())).Returns(GetRecipes());
+        //    mockRepo.Setup(repo => repo.Recipes.Delete(GetRecipe()));
+        //    //var controllerActionResult = recipeController.DeleteRecipe(It.IsAny<int>());
+        //    Assert.Throws<NoElementsException>((() => recipeController.DeleteRecipe(It.IsAny<int>())));
+        //    //Assert.IsType<NotFoundResult>(controllerActionResult);
+        //    //mockRepo.Setup(repo => repo.Recipes.FindByCondition(c => c.Id == It.IsAny<int>())).Returns(GetRecipes());
+        //    //Assert.NotNull(controllerActionResult);
+        //}
         private IEnumerable<Recipe> GetRecipes()
         {
             var recipes = new List<Recipe> {
-                new Recipe(){Id=1, Title="recipe1", Method="Cook", Ingredients = "Food", PictureUrl = "", Type = DietType.Vegan},
-                new Recipe(){Id=2, Title="recipe2", Method="Bake", Ingredients = "Vegs", PictureUrl = "", Type = DietType.Vegetarian}
-                };
+            new Recipe(){Id=1, Title = "Recipe", Method = "Cook", Ingredients = "Food", Type = DietType.Vegan, PictureUrl = ""},
+            new Recipe(){Id=2, Title = "Another recipe", Method = "Bake", Ingredients = "More food", Type = DietType.Vegetarian, PictureUrl = "" }
+            };
             return recipes;
         }
-        //private Recipe GetRecipe()
-        //{
-        //    return GetRecipes().ToList()[0];
-        //}
-        //}
-        /// <summary>
-        /// /////////////////////
-        /// </summary>
-        [Fact]
-        public void Index()
+        private Recipe GetRecipe()
         {
+            return GetRecipes().ToList()[0];
         }
 
-        [Fact]
-        public void DetailsOfRecipe()
-        {
-        }
-
-        [Fact]
-        public void UpdateRecipe()
-        {
-        }
-        [Fact]
-        public void DeleteRecipe()
-        {
-        }
     }
 }
---
+++ CrudDietApp/CrudDietTests/Controllers/UserControllerTest.cs
@@ -0,0 +1,96 @@
+﻿using CrudDietApp.Controllers;
+using CrudDietLibrary.Interfaces;
+using CrudDietLibrary.Models;
+using CrudDietLibrary.Models.Binding;
+using CrudDietLibrary.Models.View;
+using Microsoft.AspNetCore.Mvc;
+using Moq;
+using Spring.Collections;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using Xunit;
+
+namespace CrudDietTests
+{
+    public class UserControllerTest
+    {
+        private Mock<IRepositoryWrapper> mockRepo;
+        private UsersController userController;
+        private AddUserBindingModel addUser; 
+        public UserControllerTest()
+        {
+            //sample models
+            addUser = new AddUserBindingModel { Username = "Anna Added", About = "Anna is nice.", Email = "anna@email.com", Password = "Password", Age = 20, PictureUrl = "" };
+
+            //controller setup
+            var userResultsMock = new Mock<IActionResult>();
+
+            mockRepo = new Mock<IRepositoryWrapper>();
+            var allUsers = GetUsers();
+            userController = new UsersController(mockRepo.Object);
+        }
+
+        [Fact]
+        public void CreateUser_Test()
+        {
+            mockRepo.Setup(repo => repo.Users.FindByCondition(c => c.Id == It.IsAny<int>())).Returns(GetUsers());
+            var controllerActionResult = userController.CreateUser(addUser);
+            Assert.NotNull(controllerActionResult);
+        }
+
+        [Fact]
+        public void GetAllUsers_Test()
+        {
+            mockRepo.Setup(repo => repo.Users.FindAll()).Returns(GetUsers());
+            var controllerActionResult = userController.Index();
+            Assert.NotNull(controllerActionResult);
+            Assert.IsType<ViewResult>(controllerActionResult);
+        }
+
+        [Fact]
+        public void GetUser_Test()
+        {
+            mockRepo.Setup(repo => repo.Users.FindAll()).Returns(GetUsers());
+            var controllerActionResult = userController.DetailsOfUser(It.IsAny<int>());
+            Assert.NotNull(controllerActionResult);
+            Assert.IsType<ViewResult>(controllerActionResult);
+        }
+
+        [Fact]
+        public void UpdateUser_Test()
+        {
+            mockRepo.Setup(repo => repo.Users.FindByCondition(r => r.Id == It.IsAny<int>())).Returns(GetUsers());
+            mockRepo.Setup(repo => repo.Users.Update(GetUser()));
+            var controllerActionResult = userController.UpdateUser(It.IsAny<int>());
+            Assert.NotNull(controllerActionResult);
+            Assert.IsType<ViewResult>(controllerActionResult);
+
+        }
+
+        //[Fact]
+        //public void DeleteUser_Test()
+        //{
+        //    mockRepo.Setup(repo => repo.Users.FindByCondition(r => r.Id == It.IsAny<int>())).Returns(GetUsers());
+        //    mockRepo.Setup(repo => repo.Users.Delete(GetUser()));
+        //    var controllerActionResult = userController.DeleteUser(It.IsAny<int>());
+        //    //Assert.Throws<NoElementsException>((() => userController.DeleteUser(It.IsAny<int>())));
+        //    //Assert.IsType<NotFoundResult>(controllerActionResult);
+        //    //mockRepo.Setup(repo => repo.Users.FindByCondition(c => c.Id == It.IsAny<int>())).Returns(GetUsers());
+        //    //Assert.Null(controllerActionResult);
+        //}
+        private IEnumerable<User> GetUsers()
+        {
+            var users = new List<User> {
+            new User(){Id=1, Username = "Anna", About = "Anna is nice.", Email = "anna@email.com", Password = "Password", Age = 20, PictureUrl = "" },
+            new User(){Id=2, Username = "Billy", About = "Billy is also nice.", Email = "billy@email.com", Password = "PasswordB", Age = 28, PictureUrl = "" }
+            };
+            return users;
+        }
+        private User GetUser()
+        {
+            return GetUsers().ToList()[0];
+        }
+
+    }
+}
